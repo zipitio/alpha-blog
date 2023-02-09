6729cadc7654064a7f1eb01b628856a5c59ebe8e
Ubuntu <ubuntu@ip-172-31-33-141.ec2.internal>
2023-02-09 19:13:10 +0000

    add BCrypt and password to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_09_153308) do
+ActiveRecord::Schema.define(version: 2023_02_09_190523) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2023_02_09_153308) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
